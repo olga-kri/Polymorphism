ede9c2f31f1d510ffd8181e34e5c79329824a8e6
olga-kri <krivenkoolga1987@gmail.com>
2023-11-27 00:05:40 +0500

    homework

---
+++ src/Truck.java
@@ -0,0 +1,17 @@
+public class Truck extends Transport {
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+    @Override
+    public void service() {
+    super.service();
+    checkEngine();
+    checkTrailer();
+    }
+}
